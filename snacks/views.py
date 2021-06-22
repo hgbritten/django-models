dd00b876e9986486fcd425cc674c1dd1e87441fd
hunter britten <huntergbritten@gmail.com>
2021-06-22 16:36:26 -0700

    all code in

---
+++ snacks/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Snack
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = Snack
+
+
+class SnackDetailView(DetailView):
+    template_name = "snack_detail.html"
+    model = Snack
